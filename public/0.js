6ae60dee29dbc4d9be5a4c2af64a545ef63add85
cloba <pjm5111@naver.com>
2019-12-20 13:29:55 +0900

    floor4 modal popup

--- public/0.js
+++ public/0.js
@@ -1,5 +1,112 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/views/Category.vue?vue&type=script&lang=js&":
+/*!**************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/views/Category.vue?vue&type=script&lang=js& ***!
+  \**************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: 'category',
+  data: function data() {
+    return {
+      categoryData: {
+        name: '',
+        imgage: ''
+      }
+    };
+  },
+  methods: {
+    attachImage: function attachImage() {// to use file reader TODO:
+    },
+    hideNewCategoryModal: function hideNewCategoryModal() {
+      this.$refs.newCategoryModal.hide();
+    },
+    showCategoryModal: function showCategoryModal() {
+      this.$refs.newCategoryModal.show();
+    },
+    createCategory: function createCategory() {
+      console.log('form submitted');
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/views/Category.vue?vue&type=template&id=6480121d&":
 /*!******************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/views/Category.vue?vue&type=template&id=6480121d& ***!
@@ -15,64 +122,167 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "container-fluid" }, [
-    _c("ol", { staticClass: "breadcrumb" }, [
-      _c(
-        "li",
-        { staticClass: "breadcrumb-item" },
-        [_c("router-link", { attrs: { to: "/" } }, [_vm._v("Dashboard")])],
-        1
-      ),
+  return _c(
+    "div",
+    { staticClass: "container-fluid" },
+    [
+      _c("ol", { staticClass: "breadcrumb" }, [
+        _c(
+          "li",
+          { staticClass: "breadcrumb-item" },
+          [_c("router-link", { attrs: { to: "/" } }, [_vm._v("Dashboard")])],
+          1
+        ),
+        _vm._v(" "),
+        _c("li", { staticClass: "breadcrumb-item active" }, [
+          _vm._v("Categories")
+        ])
+      ]),
       _vm._v(" "),
-      _c("li", { staticClass: "breadcrumb-item active" }, [
-        _vm._v("Categories")
-      ])
-    ]),
-    _vm._v(" "),
-    _vm._m(0)
-  ])
+      _c("div", { staticClass: "card mb-3" }, [
+        _c("div", { staticClass: "card-header d-flex" }, [
+          _vm._m(0),
+          _vm._v(" "),
+          _c(
+            "button",
+            {
+              staticClass: "btn btn-primary btn-sm ml-auto",
+              on: { click: _vm.showCategoryModal }
+            },
+            [
+              _c("span", { staticClass: "fa fa-plus" }),
+              _vm._v(" Create New\n            ")
+            ]
+          )
+        ]),
+        _vm._v(" "),
+        _vm._m(1)
+      ]),
+      _vm._v(" "),
+      _c(
+        "b-modal",
+        {
+          ref: "newCategoryModal",
+          attrs: { "hide-footer": "", title: "Add New Category" }
+        },
+        [
+          _c("div", { staticClass: "d-block" }, [
+            _c("form", { on: { "submit:prevent": _vm.createCategory } }, [
+              _c("div", { staticClass: "form-group" }, [
+                _c("label", { attrs: { for: "name" } }, [_vm._v("Enter Name")]),
+                _vm._v(" "),
+                _c("input", {
+                  directives: [
+                    {
+                      name: "model",
+                      rawName: "v-model",
+                      value: _vm.categoryData.name,
+                      expression: "categoryData.name"
+                    }
+                  ],
+                  staticClass: "form-control",
+                  attrs: {
+                    type: "email",
+                    id: "name",
+                    placeholder: "Enter category name"
+                  },
+                  domProps: { value: _vm.categoryData.name },
+                  on: {
+                    input: function($event) {
+                      if ($event.target.composing) {
+                        return
+                      }
+                      _vm.$set(_vm.categoryData, "name", $event.target.value)
+                    }
+                  }
+                })
+              ]),
+              _vm._v(" "),
+              _c("div", { staticClass: "form-group" }, [
+                _c("label", { attrs: { for: "name" } }, [
+                  _vm._v("Choose an image")
+                ]),
+                _vm._v(" "),
+                _c("input", {
+                  staticClass: "form-control",
+                  attrs: { type: "file", change: "attachImage", id: "name" },
+                  on: { "": function($event) {} }
+                })
+              ]),
+              _vm._v(" "),
+              _c("hr"),
+              _vm._v(" "),
+              _c("div", { staticClass: "text-right" }, [
+                _c(
+                  "button",
+                  {
+                    staticClass: "btn btn-default",
+                    attrs: { type: "button" },
+                    on: { click: _vm.hideNewCategoryModal }
+                  },
+                  [_vm._v("Cancel")]
+                ),
+                _vm._v(" "),
+                _c(
+                  "button",
+                  { staticClass: "btn btn-primary", attrs: { type: "submit" } },
+                  [
+                    _c("span", { staticClass: "fa fa-check" }),
+                    _vm._v(" Save\n                    ")
+                  ]
+                )
+              ])
+            ])
+          ])
+        ]
+      )
+    ],
+    1
+  )
 }
 var staticRenderFns = [
   function() {
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "card mb-3" }, [
-      _c("div", { staticClass: "card-header" }, [
-        _c("i", { staticClass: "fas fa-chart-area" }),
-        _vm._v("\n            Categories Management\n        ")
-      ]),
-      _vm._v(" "),
-      _c("div", { staticClass: "card-body" }, [
-        _c("table", { staticClass: "table" }, [
-          _c("thead", [
-            _c("tr", [
-              _c("td", [_vm._v("ID")]),
+    return _c("span", [
+      _c("i", { staticClass: "fas fa-chart-area" }),
+      _vm._v("\n                Categories Management\n            ")
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "card-body" }, [
+      _c("table", { staticClass: "table" }, [
+        _c("thead", [
+          _c("tr", [
+            _c("td", [_vm._v("ID")]),
+            _vm._v(" "),
+            _c("td", [_vm._v("Name")]),
+            _vm._v(" "),
+            _c("td", [_vm._v("Image")]),
+            _vm._v(" "),
+            _c("td", [_vm._v("Action")])
+          ])
+        ]),
+        _vm._v(" "),
+        _c("tbody", [
+          _c("tr", [
+            _c("td", [_vm._v("1")]),
+            _vm._v(" "),
+            _c("td", [_vm._v("shirt")]),
+            _vm._v(" "),
+            _c("td", [_vm._v("image")]),
+            _vm._v(" "),
+            _c("td", [
+              _c("button", { staticClass: "btn btn-primary btn-sm" }, [
+                _c("span", { staticClass: "fa fa-edit" })
+              ]),
               _vm._v(" "),
-              _c("td", [_vm._v("Name")]),
-              _vm._v(" "),
-              _c("td", [_vm._v("Image")]),
-              _vm._v(" "),
-              _c("td", [_vm._v("Action")])
-            ])
-          ]),
-          _vm._v(" "),
-          _c("tbody", [
-            _c("tr", [
-              _c("td", [_vm._v("1")]),
-              _vm._v(" "),
-              _c("td", [_vm._v("shirt")]),
-              _vm._v(" "),
-              _c("td", [_vm._v("image")]),
-              _vm._v(" "),
-              _c("td", [
-                _c("button", { staticClass: "btn btn-primary btn-sm" }, [
-                  _c("span", { staticClass: "fa fa-edit" })
-                ]),
-                _vm._v(" "),
-                _c("button", { staticClass: "btn btn-danger btn-sm" }, [
-                  _c("span", { staticClass: "fa fa-trash" })
-                ])
+              _c("button", { staticClass: "btn btn-danger btn-sm" }, [
+                _c("span", { staticClass: "fa fa-trash" })
               ])
             ])
           ])
@@ -97,15 +307,17 @@ render._withStripped = true
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Category_vue_vue_type_template_id_6480121d___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Category.vue?vue&type=template&id=6480121d& */ "./resources/js/views/Category.vue?vue&type=template&id=6480121d&");
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* harmony import */ var _Category_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Category.vue?vue&type=script&lang=js& */ "./resources/js/views/Category.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
-var script = {}
+
+
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
-  script,
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _Category_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _Category_vue_vue_type_template_id_6480121d___WEBPACK_IMPORTED_MODULE_0__["render"],
   _Category_vue_vue_type_template_id_6480121d___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
@@ -119,6 +331,20 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
 if (false) { var api; }
 component.options.__file = "resources/js/views/Category.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/views/Category.vue?vue&type=script&lang=js&":
+/*!******************************************************************!*\
+  !*** ./resources/js/views/Category.vue?vue&type=script&lang=js& ***!
+  \******************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Category_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./Category.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/views/Category.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Category_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/views/Category.vue
+++ resources/js/views/Category.vue
@@ -8,9 +8,14 @@
         </ol>
 
         <div class="card mb-3">
-            <div class="card-header">
-                <i class="fas fa-chart-area"></i>
-                Categories Management
+            <div class="card-header d-flex">
+                <span>
+                    <i class="fas fa-chart-area"></i>
+                    Categories Management
+                </span>
+                <button class="btn btn-primary btn-sm ml-auto" v-on:click="showCategoryModal">
+                    <span class="fa fa-plus"></span> Create New
+                </button>
             </div>
             <div class="card-body">
                 <table class="table">
@@ -36,6 +41,63 @@
                 </table>
             </div>
         </div>
+
+        <b-modal ref="newCategoryModal" hide-footer title="Add New Category">
+            <div class="d-block">
+                <form v-on:submit:prevent="createCategory">
+                    <div class="form-group">
+                        <label for="name">Enter Name</label>
+                        <input type="email" v-model="categoryData.name" class="form-control" id="name"
+                        placeholder="Enter category name">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Choose an image</label>
+                        <input type="file" v-on: change="attachImage" class="form-control" id="name">
+                    </div>
+
+                    <hr>
+                    <div class="text-right">
+                        <button type="button" class="btn btn-default" v-on:click="hideNewCategoryModal">Cancel</button>
+
+                        <button type="submit" class="btn btn-primary">
+                            <span class="fa fa-check"></span> Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </b-modal>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'category',
+        data() {
+            return {
+                categoryData: {
+                    name: '',
+                    imgage: '',
+                }
+            }
+        },
+        methods: {
+            attachImage() {
+                // to use file reader TODO:
+            },
+
+            hideNewCategoryModal() {
+                this.$refs.newCategoryModal.hide();
+            },
+
+            showCategoryModal() {
+                this.$refs.newCategoryModal.show();
+            },
+
+            createCategory() {
+                console.log('form submitted')
+            }
+
+        }
+    }
+</script>
 
